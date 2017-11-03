0fd7cbd714a421d7531561cca0a3ae701de1358c
huangkun <jack-huang-developer@foxmail.com>
2017-11-03 19:21:45 +0800

    Initial Commit

---
+++ WebBugDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WebBugDemo
+//
+//  Created by huangkun on 2017/11/3.
+//  Copyright © 2017年 huangkun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WebBugDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WebBugDemo
+//
+//  Created by huangkun on 2017/11/3.
+//  Copyright © 2017年 huangkun. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
